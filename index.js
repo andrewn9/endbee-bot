0ffe1e55df7f6e2102b4d1801f0ff7f891b78cf0
Andrew <ng9417265@gmail.com>
2023-03-06 19:39:00 -0500

    added message reading

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Client, Intents } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
 const config = require('./config.json');
+const games = require('./classes/gamemanager.js');
 
 const client = new Client({
   intents: [
@@ -63,5 +64,31 @@ client.on('interactionCreate', async (interaction) => {
     await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
   }
 });
+
+client.on('messageCreate', async message => {
+  if (message.author.bot || message.channel.type !== 'GUILD_PUBLIC_THREAD') {
+    return;
+  }
+
+  // Get the game instance for the thread
+  const game = games.get(message.channel.id);
+
+  // Ignore messages from users not in the game
+  if (!game || !game.players.find(p => p.id === message.author.id)) {
+    return;
+  }
+
+  // Ignore messages with invalid content (i.e. not a single letter)
+  const letter = message.content.trim().toUpperCase();
+  if (!letter || letter.length !== 1 || !/^[A-Z]$/.test(letter)) {
+    console.log("not a letter");
+    return;
+  }
+
+  // Update the current word and print the updated info
+  await game.updateCurrentWord(letter);
+  game.printInfo();
+});
+
 
 client.login(process.env.TOKEN);
