786c75620d9ae855f85310a614d969a7c4dbc3ea
Andrew <79771471+anglit@users.noreply.github.com>
2023-03-09 19:59:25 +0000

    switched to datamuse api

--- commands/search.js
+++ commands/search.js
@@ -3,56 +3,34 @@ const env = require('dotenv').config();
 
 module.exports = {
   execute: async function(interaction) {
-    var search = interaction.options.getString('query');
-    
-    var addition = "";
-    var searchLength = search.length-1;
-
-    for(var i = 0; i < searchLength; i++)
-    {
-      if(i%2==0)
-        addition+="_";
-      else
-        addition+="+";
-    }
-
-    if(searchLength<=2)
-      addition = "";
-
-    search += addition;
-    console.log(`modified word: '${search}'.`);
-    
+    const search = interaction.options.getString('query');
     
     if (!search) {
       await interaction.reply('No search term was specified!');
       return;
     }
-
-    const apiKey = process.env.MERRIAM_KEY;
-    const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${search}?key=${apiKey}`;
-    search = interaction.options.getString('query');
+    
+    //const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${search}?key=${apiKey}`;
+    const url = `https://api.datamuse.com/words?sp=${search}*&max=20`;
     console.log(url);
 
+    
     https.get(url, (response) => {
-      let data = '';
+      let data = ''; 
 
       response.on('data', (chunk) => {
         data += chunk;
       });
 
       response.on('end', () => {
+        console.log(data);
         const entries = JSON.parse(data);
-
-        const exactMatches = entries.filter(entry => entry.slice(0, search.length) === search);
+        console.log(entries);
 
         let messageContent;
-        if (exactMatches.length === 0) {
-          messageContent = `No words found that begin exactly with '${search}'.`;
-        } else {
-          messageContent = `Words that begin exactly with '${search}':\n`;
-          messageContent += exactMatches.join('\n');
-        }
-
+        messageContent = `Words that begin exactly with '${search}':\n`;
+        messageContent += entries.join('\n');
+        
         interaction.reply(messageContent);
       });
     });
